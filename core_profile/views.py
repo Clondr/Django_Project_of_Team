ef597e12443825c012201e298be47c0fa86b0b91
Clondr <kirilleb09@gmail.com>
2026-08-06 15:54:09 +0300

    Исправление багов с профилем.

--- core_profile/views.py
+++ core_profile/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-
 from .models import Profile
 from django.db.models import Avg
+from .forms import *
 # Create your views here.
 
 
